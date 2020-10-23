31632c0ee089199a144aa66466b23a4446336461
hoan.nguyen <hoan.nguyen@waverleysoftware.com>
2020-10-23 14:27:44 +0700

    add order now btn

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,28 @@
-import React from 'react'; 
+import React from 'react';
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+]
 
 const buildControls = (props) => (
-    <div>
-        
+    <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        { controls.map(ctrl => (
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label} 
+                added={() => props.ingredientsAdded(ctrl.type)}
+                removed={() => props.ingredientsRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}
+                />
+        )
+        )}
+        <button disabled={!props.purchasable} className={classes.OrderButton}>ORDER NOW</button>
     </div>
 );
 
