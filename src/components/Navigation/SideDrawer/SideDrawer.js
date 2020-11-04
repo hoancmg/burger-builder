f10095042e01884491e7816998bf4eaa074e5edc
hoan.nguyen <hoan.nguyen@waverleysoftware.com>
2020-11-04 15:50:28 +0700

    finished

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDraw.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Auxx';
+import Aux from '../../../hoc/Auxx/Auxx';
 
 const sideDrawer = (props) => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
